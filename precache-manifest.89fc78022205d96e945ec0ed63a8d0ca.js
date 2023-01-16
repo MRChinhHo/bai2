8409b03e6068df67a780e676d61c9c6fd8db43c4
unknown <chinhhqfx16046@funix.edu.vn>
2023-01-16 07:01:22 +0700

    project

---
+++ precache-manifest.89fc78022205d96e945ec0ed63a8d0ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665b7b62bc7a8b352f5aa1578bdb035b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a284126d3e08d7e585",
+    "url": "/static/css/main.2aec092d.chunk.css"
+  },
+  {
+    "revision": "145fc0e427c886e9f46f",
+    "url": "/static/js/2.a42d187f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.a42d187f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a284126d3e08d7e585",
+    "url": "/static/js/main.5e3352a8.chunk.js"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
